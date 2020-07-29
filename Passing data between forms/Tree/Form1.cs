5b4df64618b791f38181a90fd56df7608051750a
Kobudzik <kobudzik98@gmail.com>
2020-07-29 16:23:18 +0200

    Added new project

---
+++ Passing data between forms/Tree/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tree
+{
+    public partial class Form1 : Form
+    {
+        //MUSI BYĆ STATYCZNE ABY NIE TRZEBA BYŁO TWORZYĆ INSTANCJI
+        public static string value;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            value = textBox1.Text;
+            SecondForm secondForm = new SecondForm();
+            secondForm.Show();
+        }
+
+        private void Form1_Enter(object sender, EventArgs e)
+        {
+         // textBox1.Text=secondForm.
+        }
+    }
+}
---
+++ Passing data between forms/Tree/SecondForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tree
+{
+    public partial class SecondForm : Form
+    {
+        public SecondForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SecondForm_Load(object sender, EventArgs e)
+        {
+            textBoxOnSecondForm.Text = Form1.value;
+        }
+    }
+}
