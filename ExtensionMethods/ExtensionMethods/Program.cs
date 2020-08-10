63e50b0f7656817c1caf3d34c8697d204f311dc5
Kobudzik <kobudzik98@gmail.com>
2020-08-10 12:09:51 +0200

    Added Extension Method proj

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string longString = "This is supposed to be a very, very long string";
+            var shortenedString = longString.Shorten(5);
+            Console.WriteLine(shortenedString);
+            Console.ReadKey();
+        }
+    }
+}
